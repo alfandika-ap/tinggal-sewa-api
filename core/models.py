fc823a5385f62b5530aec62896385f3f2f342ed0
wilganfauzan <wilganfauzan93@gmail.com>
2025-05-14 15:11:42 +0700

    Make Crawl Models

--- core/models.py
+++ core/models.py
@@ -13,3 +13,23 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+clas Page(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=255, blank=True, null=True)
+    keyword = models.CharField(max_length=255, blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title or self.url
+
+class Image(models.Model):
+    page = models.ForeignKey(Page, related_name='images', on_delete=models.CASCADE)
+    image_file = models.ImageField(upload_to='images/')
+    alt_text = models.CharField(max_length=255, blank=True, null=True)
+    downloaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image_file.url
