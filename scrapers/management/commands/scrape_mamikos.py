83f56b1ff764323fd262727b0c7cd370fbf0d946
wilganfauzan <wilganfauzan93@gmail.com>
2025-05-17 08:35:50 +0700

    Add embedding function

--- scrapers/management/commands/scrape_mamikos.py
+++ scrapers/management/commands/scrape_mamikos.py
@@ -1,18 +1,36 @@
 import asyncio
-from crawl4ai import AsyncCrawler, BrowserConfig, ProxyConfig
-from models import Property
+from crawl4ai import AsyncWebCrawler, BrowserConfig, ProxyConfig
+
+from django.core.management.base import BaseCommand
+from scrapers.models import Property
+
+import chromadb
+from chromadb.utils.embedding_functions import ONNXMiniLM_L6_V2
 
 from core.ai.proxy import config
-from core.ai.scrapper import client
+from core.ai.scraper import client
 
 proxy = ProxyConfig(**config)
 
-browser_config = BrowserConfig(
+config = BrowserConfig(
     proxy=proxy
 )
 
+class Command(BaseCommand):
+    help = "Scrape properties from mamikos.com"
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Running Mamikos scraper...')
+        asyncio.run(main())
+
+
 async def main():
-    async with AsyncCrawler(browser_config=browser_config) as crawler:
+    chroma = await chromadb.AsyncHttpClient("localhost", 8010)
+    collection = await chroma.create_collection(
+        name="property_data",
+        embedding_function=ONNXMiniLM_L6_V2(),
+    )
+    async with AsyncWebCrawler(config=config) as crawler:
         result = await crawler.arun("https://mamikos.com/cari?rent=2&sort=price,-&price=10000-20000000&singgahsini=0")
 
         res = await client.chat.completions.create(
@@ -40,20 +58,13 @@ async def main():
 
             property_data = res.choices[0].message.parsed
 
-            with open("property.json", "a") as f:
-                f.write(str(property_data) + "\n")
-
-
-            await Property.objects.create(
-                property_id=property_data["property_id"],
-                title=property_data["title"],
-                price=property_data["price"],
-                location=property_data["location"],
-                rules=property_data["rules"],
-                room_specs=property_data["room_specs"],
-                url=property_data["url"],
+            await collection.add(
+                documents=[str(property_data.model_dump())],
+                ids=[property_data.url],
+                metadatas=[
+                    {
+                    "source": "mamikos",
+                    "url": property_data.url,
+                    }
+                ],
             )
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
